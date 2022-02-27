acbcfa23f13dac73df3ce6fc55da75103d01781a
J.P. Zambrano <jpzambrano95@hotmail.com>
2022-02-27 16:49:09 -0600

    Normalizes story component styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,25 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/stories/**/*.stories.mdx",
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-scss"
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
+  webpackFinal: (config) => {
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    );
+
+    return config;
+  }
 }

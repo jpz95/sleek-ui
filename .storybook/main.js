a6f9a33b2720b6ed6c9b44c1996a53e7c14f427a
J.P. Zambrano <jpzambrano95@hotmail.com>
2022-02-28 00:21:03 -0600

    Adds formatting/linting

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,22 +4,22 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 module.exports = {
   stories: [
     "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-scss"
+    "@storybook/preset-scss",
   ],
   framework: "@storybook/react",
   webpackFinal: (config) => {
     config.resolve.plugins.push(
       new TsconfigPathsPlugin({
         configFile: path.resolve(__dirname, "../tsconfig.json"),
-      }),
+      })
     );
 
     return config;
-  }
-}
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import "@/styles/index.scss"
+import "@/styles/index.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,4 +8,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
